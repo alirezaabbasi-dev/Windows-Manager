dbdc84c05c45f92b8653595062cd0d8919239b91
alirezaabbasi-dev <alirezaabbasi001@outlook.com>
2023-05-21 13:04:06 +0330

    V.2.0.0 Release latest build

--- YP Windows Manager(Laptop)/Properties/AssemblyInfo.cs
+++ YP Windows Manager(Laptop)/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YP Windows Manager(Computer)")]
+[assembly: AssemblyTitle("YP Windows Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("YP Windows Manager(Computer)")]
-[assembly: AssemblyCopyright("YP Copyright © 2019")]
+[assembly: AssemblyProduct("YP Windows Manager")]
+[assembly: AssemblyCopyright("YP Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
